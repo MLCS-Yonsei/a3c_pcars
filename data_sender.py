b143a4f33a3b037161bdb46eae5cdf6f5040ff96
jhpark0302 <jhpark0302@github.com>
2018-06-22 11:22:07 +0900

    data_sender

--- data_sender.py
+++ data_sender.py
@@ -11,16 +11,16 @@ import redis
 import json
 import time
 
-# import win32gui
-# import win32ui
-# import win32con
+import win32gui
+import win32ui
+import win32con
 
 from PIL import Image
 import numpy as np
 
 from threading import Thread
 
-from pcars_stream.src.stream import PCarsStreamReceiver
+from pcars_stream.src.pcars.stream import PCarsStreamReceiver
 
 import http.client
 import socket 
@@ -31,59 +31,16 @@ local_ip = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not
 r = redis.StrictRedis(host='lab.hwanmoo.kr', port=6379, db=1)
 
 ''' CREST Thread '''
-class MyPCarsListener(object):
+class PCarsListener(object):
+    def __init__(self):
+        self.data = None
+
     def handlePacket(self, data):
         # You probably want to do something more exciting here
         # You probably also want to switch on data.packetType
         # See listings in packet.py for packet types and available fields for each
-        print(data)
-
-class crest_thread(Thread):        
-
-    def __init__(self):
-        self.crest_data = None
-        # self.listener = MyPCarsListener()
-        super(crest_thread, self).__init__()
-
-    def send_crest_requset(self, url, flag, option):
-        conn = http.client.HTTPConnection(url, timeout=1)
-        try:
-            conn.request("GET", "/crest/v1/api")
-
-            res = conn.getresponse()
-
-            data = json.loads(res.read().decode('utf8', "ignore").replace("'", '"'))
-
-            if data["gameStates"]["mGameState"] > 1:
-                if flag == 'crest-monitor':
-                    return data
-        except Exception as e:
-            print("CREST_ERROR on send_crest_request:", e)
-            return False
-
-    # def run(self):
-    #     try:
-    #         ''' Try 8080 port to get crest data '''
-    #         crest_data = self.send_crest_requset('localhost:8080', "crest-monitor", {})
-    #         gameState = crest_data['gameStates']['mGameState']
-
-    #         if gameState > 1 and 'participants' in crest_data:
-    #             if 'mParticipantInfo' in crest_data["participants"]:
-    #                 # 게임 플레이중
-    #                 self.crest_data = crest_data
-    #         else:
-    #             self.crest_data = False
-
-    #     except Exception as e:
-    #         print(e)
-    #         self.crest_data = False
-
-    def run(self):
-        pass
-
-
-
-
+        # print(data._data)
+        self.data = data._data
 
 class screen_capture_thread(Thread):        
 
@@ -92,62 +49,63 @@ class screen_capture_thread(Thread):
         super(screen_capture_thread, self).__init__()
 
     def run(self):
-        # # Get Focus on project cars window
-        # windowname = "Project CARS™"
-        # hwnd = win32gui.FindWindow(None, windowname)
+        # Get Focus on project cars window
+        windowname = "Project CARS™"
+        hwnd = win32gui.FindWindow(None, windowname)
         
-        # # Get window properties and take screen capture
-        # l, t, r, b = win32gui.GetWindowRect(hwnd)
-        # w = r - l
-        # h = b - t
-        # wDC = win32gui.GetWindowDC(hwnd)
-        # dcObj=win32ui.CreateDCFromHandle(wDC)
-        # cDC=dcObj.CreateCompatibleDC()
-        # dataBitMap = win32ui.CreateBitmap()
-        # dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
-        # cDC.SelectObject(dataBitMap)
-        # cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
-        # # dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+        # Get window properties and take screen capture
+        l, t, r, b = win32gui.GetWindowRect(hwnd)
+        w = r - l
+        h = b - t
+        wDC = win32gui.GetWindowDC(hwnd)
+        dcObj=win32ui.CreateDCFromHandle(wDC)
+        cDC=dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
+        # dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
 
-        # bmpinfo = dataBitMap.GetInfo()
-        # bmpstr = dataBitMap.GetBitmapBits(True)
+        bmpinfo = dataBitMap.GetInfo()
+        bmpstr = dataBitMap.GetBitmapBits(True)
 
-        # im = Image.frombuffer('RGB', (bmpinfo['bmWidth'], bmpinfo['bmHeight']), bmpstr, 'raw', 'BGRX', 0, 1)
+        im = Image.frombuffer('RGB', (bmpinfo['bmWidth'], bmpinfo['bmHeight']), bmpstr, 'raw', 'BGRX', 0, 1)
 
-        # self.img = np.array(im)
+        self.img = np.array(im)
 
-        # # Free Resources
-        # dcObj.DeleteDC()
-        # cDC.DeleteDC()
-        # win32gui.ReleaseDC(hwnd, wDC)
-        # win32gui.DeleteObject(dataBitMap.GetHandle())
-        pass
+        # Free Resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+        
 
 
 if __name__ == '__main__':
     print('Starting Data Sender.. [A3C on Project Cars]')
-    listener = MyPCarsListener()
+    ''' 
+    Listening Pcars UDP port 
+    Make sure to set udp setting in the game option as 1
+    '''
+    listener = PCarsListener()
     stream = PCarsStreamReceiver()
     stream.addListener(listener)
     stream.start()
+    
     while True:
-        # Getting Data from CREST API
-        ct = crest_thread()
-        ct.daemon = True 
-        ct.start()
-
         # Taking Screen Capture form Pcars
         sct = screen_capture_thread()
         sct.daemon = True 
         sct.start()
 
-        ct.join()
         sct.join()
-
-        if ct.crest_data is not False and sct.img is not None:
-            result = {'game_data':ct.crest_data,'image_data':sct.img}
+        
+        # Set game_data from pcars udp listener after taking screen capturing
+        if listener.data is not False and sct.img is not None:
+            result = {'game_data':listener.data,'image_data':sct.img}
         else:
             result = False
+
         r.hdel('pcars_data',local_ip)
         r.hset('pcars_data',local_ip,result)
         
