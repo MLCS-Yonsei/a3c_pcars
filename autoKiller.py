00bda39c2c2ac0ed4809755c02878014956bf18f
codemoo <hwanmooy@gmail.com>
2018-06-27 18:32:39 +0900

    f

--- autoKiller.py
+++ autoKiller.py
@@ -105,6 +105,8 @@ class pCarsAutoKiller(mp.Process):
         pywinauto.mouse.move(coords=(x+30, y))
         time.sleep(1)
         pywinauto.mouse.press(button='left', coords=(x+30, y+90))
+        pywinauto.mouse.press(button='left', coords=(x+30, y+90))
+        pywinauto.mouse.press(button='left', coords=(x+30, y+90))
         # pywinauto.mouse.release(button='left', coords=(x+30, y))
 
     def restart_type_1(self):
