1448207ec2e35dc9ee94a17ab0e3f6cf06524bad
codemoo <hwanmooy@gmail.com>
2018-06-28 10:32:49 +0900

    f

--- vm_autoKiller.py
+++ vm_autoKiller.py
@@ -72,10 +72,7 @@ class pCarsAutoKiller(mp.Process):
         target_name = "DESKTOP-FOKU7V8의 V" + str(vpc_idx+1) + " - 가상 컴퓨터 연결"
         print(target_name)
         PyCWnd1 = win32ui.FindWindow( None, target_name )
-        try:
-            PyCWnd1.SetForegroundWindow()
-        except:
-            pass
+        PyCWnd1.SetForegroundWindow()
         PyCWnd1.SetFocus()
 
     def trigger_arduino_esc(self, vpc_idx):
