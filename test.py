12a5ee57dd0c21d9c8ca28dbf2da7acfe9da3d5a
Hwanmoo Yong <hwanmooy@gmail.com>
2018-07-11 16:22:31 +0900

    f

--- test.py
+++ test.py
@@ -3,6 +3,21 @@ import win32ui
 windowname = "Project CARS™"
 bmpfilenamename = "123.bmp"
 hwnd = win32gui.FindWindow(None, windowname)
+
+l, t, _r, b = win32gui.GetWindowRect(hwnd)
+
+target_w = 800
+target_h = 600
+
+margin_w  = int((_r-l-target_w) / 2)
+
+l = l + margin_w
+_r = _r - margin_w
+b = b - margin_w
+t = t + ((b-t-target_h))
+w = _r - l
+h = b - t
+
 wDC = win32gui.GetWindowDC(hwnd)
 dcObj=win32ui.CreateDCFromHandle(wDC)
 cDC=dcObj.CreateCompatibleDC()
