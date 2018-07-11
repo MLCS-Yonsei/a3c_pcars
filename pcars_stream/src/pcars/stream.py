5aa120a10b091093fa973bbe070b49c2d5e16e90
Hwanmoo Yong <hwanmooy@gmail.com>
2018-07-11 17:20:43 +0900

    f

--- pcars_stream/src/pcars/stream.py
+++ pcars_stream/src/pcars/stream.py
@@ -29,7 +29,7 @@ class PCarsStreamReceiver(Thread):
         # sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
         
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(("127.0.0.1", self.port))
+        sock.bind(("192.168.0.49", self.port))
 
         while True:
             try:
