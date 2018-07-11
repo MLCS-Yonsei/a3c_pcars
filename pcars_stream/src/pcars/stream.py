7c9aee3dd493a588233d27182809c5b45f510aeb
Hwanmoo Yong <hwanmooy@gmail.com>
2018-07-11 17:35:39 +0900

    f

--- pcars_stream/src/pcars/stream.py
+++ pcars_stream/src/pcars/stream.py
@@ -5,8 +5,8 @@ import socket
 import struct
 
 
-_MCAST_ANY = "127.0.0.1"
-print("_MCAST_ANY", _MCAST_ANY)
+_MCAST_ANY = "224.0.0.1"
+
 
 class PCarsStreamReceiver(Thread):
 
@@ -20,21 +20,17 @@ class PCarsStreamReceiver(Thread):
         self.listeners.append(listener)
 
     def run(self):
-        sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         # Bind to the server address
-        # sock.bind(("", self.port))
-        # group = socket.inet_aton(_MCAST_ANY)
-        # mreq = struct.pack("4sL", group, socket.INADDR_ANY)
-        # sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-        
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(("localhost", self.port))
+        sock.bind(("", self.port))
+        group = socket.inet_aton(_MCAST_ANY)
+        mreq = struct.pack("4sL", group, socket.INADDR_ANY)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
         while True:
             try:
                 data = sock.recv(1400)
-                # print(data)
                 packet = Packet.readFrom(BytesIO(data))
                 for listener in self.listeners:
                     listener.handlePacket(packet)
