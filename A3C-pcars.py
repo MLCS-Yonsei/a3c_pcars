2f54ccb82470251648b7a1d859dac7a28c2c47ee
Hwanmoo Yong <hwanmooy@gmail.com>
2018-07-12 22:01:36 +0900

    f

--- A3C-pcars.py
+++ A3C-pcars.py
@@ -551,8 +551,8 @@ if __name__ == "__main__":
         os.makedirs('./frames')
 
     if len(sys.argv) == 1:  # run from PyCharm
-        play_training(training=True, load_model=True)
+        play_training(training=True, load_model=False)
     elif sys.argv[1] == "1":  # lunch from Terminal and specify 0 or 1 as arguments
-        play_training(training=True, load_model=True)
+        play_training(training=True, load_model=False)
     elif sys.argv[1] == "0":
         play_training(training=False, load_model=True)
