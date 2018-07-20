e1b346f9de478e15e274288db6b2835669d2db6a
Hwanmoo Yong <hwanmooy@gmail.com>
2018-07-20 17:36:54 +0900

    ㄹ

--- gym_pcars.py
+++ gym_pcars.py
@@ -331,7 +331,7 @@ class PcarsEnv:
                 
             print("reward:",self.reward,target_ip)
 
-            if self.reward <= -300 and terminate_status is False:
+            if self.reward <= -500 and terminate_status is False:
                 print("Restarting by finish")
                 self.brake_cnt = 0
                 self.stop_cnt = 0
