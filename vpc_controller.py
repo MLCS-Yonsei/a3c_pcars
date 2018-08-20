5a04a5993041a6d589b57d8f25f49cba9ecc2e6d
Hwanmoo Yong <hwanmooy@gmail.com>
2018-08-20 14:21:32 +0900

    f

--- vpc_controller.py
+++ vpc_controller.py
@@ -117,7 +117,7 @@ class screen_capture_thread(Thread):
                             # message = message.replace('>','>\'')
 
                             # ob = eval(self.listener)
-                            ob = self.listener
+                            ob = self.listener.data
 
                             gameState = [int(s) for s in ob["gameState"].split('>')[0].split() if s.isdigit()][0]
                             raceState = [int(s) for s in ob["raceState"].split('>')[0].split() if s.isdigit()][0]
