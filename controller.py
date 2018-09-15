e202a4b53e7d59e771fc4341d2297984a964d6eb
Hwanmoo Yong <hwanmooy@gmail.com>
2018-09-15 17:29:57 +0900

    f

--- controller.py
+++ controller.py
@@ -193,36 +193,38 @@ def run_pac(r, local_ip):
 def run_pkr(r, local_ip):
     pc = pCarsAutoKiller()
     while True:
+        try:
+            message = r.hget('pcars_killer'+local_ip,local_ip)
+            
+            if message:
+                reset_status = eval(message)
+                print(reset_status == 0)
+                if reset_status == 1:
+                    pc.restart_type_1()
+                    del_stat = True
+                elif reset_status == 2:
+                    pc.restart_type_2()
+                    del_stat = True
+                elif reset_status == 3:
+                    pc.restart_type_3()
+                    del_stat = True
+                elif reset_status == 4:
+                    pc.restart_type_4()
+                    print("RST 4 done")
+                    del_stat = False
+                    r.hset('pcars_killer'+local_ip,local_ip,"0")
+                elif reset_status == 0:
+                    del_stat = False
+                    # time.sleep(0.3)
+                else:
+                    del_stat = True
 
-        message = r.hget('pcars_killer'+local_ip,local_ip)
-        
-        if message:
-            reset_status = eval(message)
-            print(reset_status == 0)
-            if reset_status == 1:
-                pc.restart_type_1()
-                del_stat = True
-            elif reset_status == 2:
-                pc.restart_type_2()
-                del_stat = True
-            elif reset_status == 3:
-                pc.restart_type_3()
-                del_stat = True
-            elif reset_status == 4:
-                pc.restart_type_4()
-                print("RST 4 done")
-                del_stat = False
-                r.hset('pcars_killer'+local_ip,local_ip,"0")
-            elif reset_status == 0:
-                del_stat = False
-                # time.sleep(0.3)
-            else:
-                del_stat = True
-
-            if del_stat:
-                r.hdel('pcars_killer'+local_ip,local_ip)
-                print("killer code removed")
-
+                if del_stat:
+                    r.hdel('pcars_killer'+local_ip,local_ip)
+                    print("killer code removed")
+        except Exception as ex:
+            print(ex)
+            time.sleep(3)
 
 if __name__ == '__main__':
     print('Starting Data Sender.. [A3C on Project Cars]')
