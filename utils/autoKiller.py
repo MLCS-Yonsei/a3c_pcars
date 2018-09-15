a47d9fc34b5570d8426a66d4799a6f3c66a6a89e
Hwanmoo Yong <hwanmooy@gmail.com>
2018-09-15 17:25:29 +0900

    f

--- utils/autoKiller.py
+++ utils/autoKiller.py
@@ -166,23 +166,102 @@ class pCarsAutoKiller(mp.Process):
         pac.accOff()
         pac.handBrakeOn()
         print("Loop start")
-        while True:
-            message = self.r.hget('pcars_data'+self.local_ip,self.local_ip)
+        time.sleep(2)
 
+        while True:
+            kill_message = self.r.hget('pcars_killer'+self.local_ip,self.local_ip)
+            if kill_message:
+                km = eval(kill_message)
+                print("Kill msg", km)
+                if km != 4:
+                    break
+
+            message = self.r.hget('pcars_data'+self.local_ip,self.local_ip)
+            # print("RST 4 : ", message)
             if message:
+                                            
+                self.r.hdel('pcars_data'+self.local_ip,self.local_ip)
+                message = message.decode("utf-8")
+                message = message.replace('<','\'<')
+                message = message.replace('>','>\'')
+
                 msg = eval(message)
                 ob = msg['game_data']
 
                 if "speed" in ob:
                     sp = ob["speed"]
-                    if sp < 0.1:
+                    print("Speed : ", sp, sp < 0.3)
+                    if sp == 0.0:
                         self.trigger_virtual_enter()
                         pac.handBrakeOff()   
-                        print("Loop escaped")
                         break
+
+                    if sp < 0.3:
+                        self.trigger_virtual_enter()
+                        if sp < 0.1:
+                            pac.handBrakeOff()   
+                            print("speed almost 0")
+                            break
+                        
+                    # pac.handBrakeOn()
+                    # print("Hand Brake On")
+                    time.sleep(0.2)
                                    
-        
+        print("Loop escaped")
         return True
+
+if __name__ == '__main__':
+    ''' Init Redis '''
+    r = redis.StrictRedis(host='redis.hwanmoo.kr', port=6379, db=1)
+
+    ''' Getting Local IP of this Computer '''
+    local_ip = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1][0]
+
+    pc = pCarsAutoKiller()
+    while True:
+        try:
+            message = r.hget('pcars_killer'+local_ip,local_ip)
+            
+            if message:
+                reset_status = eval(message)
+                print(reset_status)
+                if reset_status == 1:
+                    pc.restart_type_1()
+                    del_stat = True
+                elif reset_status == 2:
+                    pc.restart_type_2()
+                    del_stat = True
+                elif reset_status == 3:
+                    pc.restart_type_3()
+                    del_stat = True
+                elif reset_status == 4:
+                    pc.restart_type_4()
+                    del_stat = False
+                    r.hset('pcars_killer'+local_ip,local_ip,"0")
+                elif reset_status == 0:
+                    del_stat = False
+                else:
+                    del_stat = True
+
+                if del_stat:
+                    r.hdel('pcars_killer'+local_ip,local_ip)
+        except:
+            pass
+            
+    # print("V esc")
+    # time.sleep(3)
+    
+    # pc.restart_type_4()
+
+                
+            
+
+            
+
+
+
+
+
 
 
 
